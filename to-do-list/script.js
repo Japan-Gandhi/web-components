a113e86df1f6c31ab42b7a2b625c24191e11aa4b
Japan-Gandhi <japanajitsingh27@gmail.com>
2024-03-23 17:05:33 +0530

    added add button functionality

---
+++ to-do-list/script.js
@@ -0,0 +1,36 @@
+
+function addTask(event) {
+  event.preventDefault();
+  var taskList = document.getElementById("task-list");
+  var textbox = document.getElementById("add-new");
+  var taskBox = document.createElement("div");
+  var newTask = document.createElement("li");
+  var newCheckbox = document.createElement("input");
+
+  if (textbox.value == "") {
+    return false;
+  }
+
+  taskBox.className = "task-box";
+
+  newCheckbox.type = "checkbox";
+  newCheckbox.id = "task-" + (taskList.children.length + 1);
+
+  var newLabel = document.createElement("label");
+
+  newLabel.setAttribute("for", newCheckbox.id);
+
+  newLabel.textContent = textbox.value;
+  
+  newTask.appendChild(newCheckbox);
+  newTask.appendChild(newLabel);
+  taskBox.appendChild(newTask);
+  taskList.appendChild(taskBox);
+  
+  textbox.value = "";
+}
+
+function deleteList()
+{
+
+}
